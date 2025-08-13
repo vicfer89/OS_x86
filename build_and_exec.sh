7af5667311bbf3df8125bf1cd09eb7838882524a
vicfer89 <vicfer89@gmail.com>
2025-08-13 17:58:12 +0200

    Creación de directorio automática para prevenir error en caso de que no exista bin o build

--- build_and_exec.sh
+++ build_and_exec.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
+BINDIR=bin
+BUILDIR=build
+
+# Comprobamos is no existe el directorio de binarios, y lo creamos
+if [ ! -d "$BINDIR" ]; then
+  echo "$BINDIR no existe, creando directorio..."
+  mkdir $BINDIR
+fi
+
+# Comprobamos is no existe el directorio de build, y lo creamos
+if [ ! -d "$BUILDIR" ]; then
+  echo "$BUILDIR no existe, creando directorio..."
+  mkdir $BUILDIR
+fi
+
 make clean && ./build.sh && qemu-system-x86_64 -hda ./bin/os.bin
